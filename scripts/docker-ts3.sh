3080f4f126dd3da66b4aed56c69513a23d9e7687
Alex <nerv@devalx.de>
2014-04-15 14:33:03 +0200

    no message

--- scripts/docker-ts3.sh
+++ scripts/docker-ts3.sh
@@ -12,11 +12,11 @@ if [ -f $VOLUME/ts3server.sqlitedb ]
   then
     echo "$VOLUME/ts3server.sqlitedb exists."
 	echo "Creating link."
-	ln -s $VOLUME/ts3server.sqlitedb /opt/teamspeak3-server_linux-amd64/ts3server.sqlitedb 
+	ln -s $VOLUME/ts3server.sqlitedb /opt/teamspeak3/ts3server.sqlitedb 
 fi
 
 
-/opt/teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh \
+/opt/teamspeak3/ts3server_minimal_runscript.sh \
   query_ip_whitelist="/teamspeak3/query_ip_whitelist.txt" \
   query_ip_backlist="/teamspeak3/query_ip_blacklist.txt" \
   logpath="/teamspeak3/logs/" \
