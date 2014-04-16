808a3bb749af30fd6d4c2f0c8e1d5156508236d9
Alex <nerv@devalx.de>
2014-04-16 11:39:55 +0200

    no message

--- scripts/docker-ts3.sh
+++ scripts/docker-ts3.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
-
 VOLUME=/teamspeak3
 
-#-- ts 3 starten im hintergrund
-#-- den erstellten files ordner symlinken
-#-- die erstelle ts3 db symlinken
-
-echo "This scripts checks the existence of the ts3server.sqlitedb."
-echo "Checking..."
+echo " ----- docker-ts3 ------"
+echo "1. Check if ts3server.sqlitedb exists in host-mounted volume."
 if [ -f $VOLUME/ts3server.sqlitedb ]
   then
-    echo "$VOLUME/ts3server.sqlitedb exists."
-	echo "Creating link."
+    echo "$VOLUME/ts3server.sqlitedb found. Creating Link between host-mounted db-file and ts3-folder."
 	ln -s $VOLUME/ts3server.sqlitedb /opt/teamspeak3-server_linux-amd64/ts3server.sqlitedb 
 fi
-
-
-/opt/teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh \
-  query_ip_whitelist="/teamspeak3/query_ip_whitelist.txt" \
-  query_ip_backlist="/teamspeak3/query_ip_blacklist.txt" \
-  logpath="/teamspeak3/logs/" \
-  licensepath="/teamspeak3/" 
-#  inifile="/teamspeak3/ts3server.ini" \
-#  createinifile=1 
-
+echo "2. Link the files-folder into the host-mounted volume. TODO Not implemented yet"
 #ln -s /opt/teamspeak3-server_linux-amd64/files/ /teamspeak3/files/
+
+
+echo "3. Starting TS3-Server."
+echo "Check if ts3server.ini exists in host-mounted volume."
+if [ -f $VOLUME/ts3server.ini ]
+  then
+    echo "$VOLUME/ts3server.ini found. Using ini as config file."
+	echo "HINT: If this ini was transfered from another ts3-install you may want to make sure the following settings are active for the usage of host-mounted volume: (OPTIONAL)"
+	echo "query_ip_whitelist='/teamspeak3/query_ip_whitelist.txt'"
+	echo "query_ip_backlist='/teamspeak3/query_ip_blacklist.txt'"
+	echo "logpath='/teamspeak3/logs/'"
+	echo "licensepath='/teamspeak3/'" 
+	echo "inifile='/teamspeak3/ts3server.ini'"
+	/opt/teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh \
+		inifile="/teamspeak3/ts3server.ini"
+  else
+	echo "$VOLUME/ts3server.ini not found. Creating new config file."
+	/opt/teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh \
+		query_ip_whitelist="/teamspeak3/query_ip_whitelist.txt" \
+		query_ip_backlist="/teamspeak3/query_ip_blacklist.txt" \
+		logpath="/teamspeak3/logs/" \
+		licensepath="/teamspeak3/" 
+		inifile="/teamspeak3/ts3server.ini" \
+		createinifile=1 
+fi
+
 
